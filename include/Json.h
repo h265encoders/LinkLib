85bbe4a1295f1bc96ca410f53c9f22efc67440be
zc <zc@linkpi.cn>
2020-06-21 14:06:20 +0800

    增加rtsp事件 优化SAVS性能 尝试加入vpss复用

--- include/Json.h
+++ include/Json.h
@@ -6,10 +6,11 @@ class Json
 {
 public:
     Json();
-    static QString encode(QVariant data);
+    static QString encode(QVariant data,bool format=true);
     static QVariant decode(const QString &jsonStr);
     static QVariant loadFile(QString path);
     static bool saveFile(QVariant data, QString path);
+    static QVariantMap join(QVariantMap A,QVariantMap B);
 };
 
 #endif // JSON_H
